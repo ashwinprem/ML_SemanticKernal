7f72777c683b5ee65cfae8670b775803a2dea643
Ashwin Prem <ashberny@gmail.com>
2025-03-17 12:03:23 -0400

    Added NuGet packages.

--- SemanticKernel/obj/Debug/net8.0/SemanticKernel.AssemblyInfo.cs
+++ SemanticKernel/obj/Debug/net8.0/SemanticKernel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ae01d8627cc6c6c820f83b3e6e494c1f00b867")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8be3a497beca5f527586899c862dc6a4468e4ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
