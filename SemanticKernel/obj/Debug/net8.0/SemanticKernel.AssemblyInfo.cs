7c0a009545f23396f6559faa5add2d08b83ccfde
Ashwin Prem <ashberny@gmail.com>
2025-03-17 13:33:46 -0400

    Updates .gitignore. Added appsettings.json

--- SemanticKernel/obj/Debug/net8.0/SemanticKernel.AssemblyInfo.cs
+++ SemanticKernel/obj/Debug/net8.0/SemanticKernel.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ed697348d7265b3c113cb5718bd2510afcd7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366f04856da92f7e08f8a11de4772f9d106a72a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemanticKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
